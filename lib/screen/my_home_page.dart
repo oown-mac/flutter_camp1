4a83a6d12fb8f681967729c7ffb184c636fb385f
oown-mac <oown.natthpon@icloud.com>
2024-05-16 16:04:31 +0700

    two page

--- lib/screen/my_home_page.dart
+++ lib/screen/my_home_page.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_camp1/screen/my_second_page.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello world 2");
+    return Scaffold(
+      drawer: const Drawer(
+        child: Text("Data1 data1 data2"),
+      ),
+      appBar: AppBar(
+        title: const Text("My Home page"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text(
+              "Hello World 2",
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                //Navigator.push(context, route)
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const MySecondPage(),
+                  ),
+                );
+              },
+              child: const Text("ไปหน้าสอง"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
